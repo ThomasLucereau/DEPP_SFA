0c54a399a0c15a8da49fc24f8cb9f6e90cce73f2
ThomasLucereau <divers.thomas.lcu@gmail.com>
2026-02-26 21:17:55 +0100

    into a library

---
+++ depp_sfa/__init__.py
@@ -0,0 +1,18 @@
+"""
+DEPP_SFA: Stochastic Frontier Analysis
+A library for MLE and Bayesian (PyMC) estimation of SFA models.
+"""
+
+__version__ = "0.1.0"
+
+from .depp_sfa import SFA
+from .constant import FUN_COST, FUN_PROD, TE_teJ, TE_te, TE_teMod
+
+__all__ = [
+    "SFA",
+    "FUN_COST",
+    "FUN_PROD",
+    "TE_teJ",
+    "TE_te",
+    "TE_teMod",
+]
