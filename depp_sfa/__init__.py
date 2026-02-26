ce54df3a2026fdb521f54e32de22edc97bc8d815
ThomasLucereau <divers.thomas.lcu@gmail.com>
2026-02-26 21:21:43 +0100

    into a library

--- depp_sfa/__init__.py
+++ depp_sfa/__init__.py
@@ -5,7 +5,7 @@ A library for MLE and Bayesian (PyMC) estimation of SFA models.
 
 __version__ = "0.1.0"
 
-from .depp_sfa import SFA
+from .depp_SFA import SFA
 from .constant import FUN_COST, FUN_PROD, TE_teJ, TE_te, TE_teMod
 
 __all__ = [
